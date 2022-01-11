3a00dd31df24fdcde5c30b8f826b86006f8ba8e4
ahsan_ullah_khan_niazi <aukniazi777@gmail.com>
2022-01-11 21:55:38 +0500

    Day2 Completed

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+ @override
+  Widget build(BuildContext context) {
+   int time= 24;
+    // TODO: implement build
+    return  Scaffold(
+      appBar: AppBar(
+          title: const Text("MIS Data"),
+      ),
+      body: Center(
+          child: Text("Allah is with us $time hours"),
+
+      ),
+      drawer: const Drawer(),
+    );
+  }
+
+}
